c24feb0fcc8153f90ee12c4436115ff3a0f2b1bf
Yoann Moinet <yoann.moinet@gmail.com>
2015-08-09 12:25:23 -0400

    feat: add the raw plugin for development

--- src/requirejs-i18njs.js
+++ src/requirejs-i18njs.js
@@ -1,9 +1,14 @@
 define({
     version: '0.0.1',
+    // Target our builder for the optimizer.
     pluginBuilder: './i18njs-builder',
     load: function (name, req, onload, config) {
-        req('json!' + name, function (raw) {
-            console.log('IS BUILD ? ' + config.isBuild ? true : false);
+        // Inject plugins.
+        config.paths.json = '../../node_modules/requirejs-plugins/src/json';
+        config.paths.text = '../../node_modules/requirejs-plugins/lib/text';
+        // Load the file as JSON
+        req(['json!' + name], function (raw) {
+            // We're in development, so we deliver the file raw.
             onload(raw);
         });
     }
